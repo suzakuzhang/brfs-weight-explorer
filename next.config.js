6b26edc06c5ac4cf11d9f36c02925c01f502ac8b
Shumin Zhang <szhang2238-c@my.cityu.edu.hk>
2026-06-23 22:06:34 +0800

    Add basePath for GitHub Pages subpath deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "/brfs-weight-explorer",
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
